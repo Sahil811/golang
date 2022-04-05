2a53f84644207367df1123bade6a8a76553de4ab
sahil <sahilsiddiqui.dev@gmail.com>
2022-04-05 12:35:37 +0530

    for loop in go

--- main.go
+++ main.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world!")
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
